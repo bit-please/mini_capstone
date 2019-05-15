6f8b34d3c4cbbfc00dc3912081d0d3e77cdbebcd
danizaghian <dani.zaghian@gmail.com>
2019-05-15 10:03:07 -0700

    adds validators to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
 
+	validates :name, presence: true, uniqueness: true
+	validates :price, numericality: {greater_than: 0}
+	validates :image_url, presence: true
+	validates :description, length: {in: 10..500}
+
 	def is_discounted?
 		price <= 10
 	end
